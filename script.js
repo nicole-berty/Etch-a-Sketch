7ffb2c1b0df5052ba72523bd5fb2d40443719d05
Nicole <nicole.berty@gmail.com>
2021-08-11 17:45:07 +0100

    Add basic grid of divs where each div changes colour to lime on hover. Add clear grid button which prompts user to choose a new grid size, erases the old grid and create a new grid of that size

---
+++ script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.container');
+
+function createGrid(gridSize) {
+    var gridArea = gridSize * gridSize;
+    for(let i = 1; i <= gridArea; i++) {
+        var gridDiv = document.createElement('div');
+        gridDiv.setAttribute("class", "gridBoxes");
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+        container.insertAdjacentElement('beforeend', gridDiv);
+    }
+
+    var gridBox = container.querySelectorAll('div');
+    gridBox.forEach(gridBox => gridBox.style.backgroundColor = "black");
+    gridBox.forEach(gridBox => gridBox.addEventListener('mouseover', addColour));
+}
+
+function addColour() {
+    this.style.backgroundColor = "lime";
+}
+
+function promptUser() {
+    do{
+        var size = parseInt(window.prompt("Enter the number of squares the new grid should have on each side - must be between 1 and 100.", ""), 10);
+    } while (isNaN(size) || size > 100 || size < 1);
+    clearGrid();
+    createGrid(size);
+}
+
+function clearGrid() {
+    var boxes = document.getElementsByClassName("gridBoxes");
+    for(var i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = 'black';
+       boxes[i].remove();
+    }
+}
+
+createGrid(16);
